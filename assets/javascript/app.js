3aa4f1940af644e59b1f8e9ba90e0cbd83757ee9
Nirav Patel <nrvpatel03@gmail.com>
2018-06-03 12:33:44 -0700

    firebase added

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBycywjAmwmc1JihB8MNAxU1Z3cHOhJtBI",
+    authDomain: "rock-paper-scissors-f82e7.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-f82e7.firebaseio.com",
+    projectId: "rock-paper-scissors-f82e7",
+    storageBucket: "rock-paper-scissors-f82e7.appspot.com",
+    messagingSenderId: "10193950721"
+  };
+  firebase.initializeApp(config);
+})
