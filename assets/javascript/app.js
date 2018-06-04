8b98b0af43c808a9b705d41846045c98dcaad913
Nirav Patel <nrvpatel03@gmail.com>
2018-06-03 19:48:36 -0700

    firebase started

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,5 +13,39 @@ $(document).ready(function(){
   //variable reference to firebase database
   var database = firebase.database();
 
-  
+  //initial values
+  var name="";
+  var wins=0;
+  var loss=0;
+
+  $("#name-input").keypress(function(event){
+    if(event.keyCode === 13){
+      event.preventDefault();
+      $("#submitButton").trigger("click");
+    }
+  })
+
+  $("#submitButton").click(function(event){
+    event.preventDefault();
+    name = $("#name-input").val();
+    $("#name-input").val("");
+    
+    database.ref("players/trash").push({name:name});
+    database.ref("players").once("value").then(function(snapshot){
+      if(snapshot.hasChild("p1") && snapshot.hasChild("p2")){
+        database.ref("players/trash").remove();
+      }else if(snapshot.hasChild("p1")){
+        database.ref("players/p2").push({name:name});
+        database.ref("players/trash").remove();
+      }else{
+        database.ref("players/p1").push({name:name});
+        database.ref("players/trash").remove();
+      }
+    })
+  })
+
+
+
+
+
 })
