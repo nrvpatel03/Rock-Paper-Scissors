9c8bb0879bd643af97d428fcfe9190a2951129f6
Nirav Patel <nrvpatel03@gmail.com>
2018-06-04 10:02:00 -0700

    firebaseContinued

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,11 +13,24 @@ $(document).ready(function(){
   //variable reference to firebase database
   var database = firebase.database();
 
+  function checkifnull(winsorloss, childSnapshot,databaseRef){
+    if(!childSnapshot.val().winsorloss){
+      winsorloss = 0;
+      databaseRef.set(winsorloss)
+    }else{
+      winsorloss = childSnapshot.val().winsorloss;
+    }
+    return winsorloss;
+  }
+
   //initial values
   var name="";
   var wins=0;
   var loss=0;
-
+  var player1="";
+  var player2="";
+  var key1;
+  var key2;
   $("#name-input").keypress(function(event){
     if(event.keyCode === 13){
       event.preventDefault();
@@ -35,18 +48,33 @@ $(document).ready(function(){
       if(snapshot.hasChild("p1") && snapshot.hasChild("p2")){
         database.ref("players/trash").remove();
       }else if(snapshot.hasChild("p1")){
-        database.ref("players/p2").push({name:name});
+        key2 = database.ref("players/p2").push({name:name}).key;
         database.ref("players/trash").remove();
       }else{
-        database.ref("players/p1").push({name:name});
+        key1 = database.ref("players/p1").push({name:name}).key;
         database.ref("players/trash").remove();
       }
     });
-    
   })
-
-
-
-
+  database.ref("players/p1").on("child_added",function(childSnapshot){
+    player1 = childSnapshot.val().name;
+    wins = checkifnull(wins,childSnapshot,database.ref("players/p1/" + key1));
+    loss = checkifnull(loss,childSnapshot,database.ref("players/p1/" + key1));
+    console.log(player1);
+    console.log(wins);
+    console.log(loss);
+  },function(errorObject){
+    console.log("errors: " + errorObject.code);
+  })
+  database.ref("players/p2").on("child_added",function(childSnapshot){
+    player2 = childSnapshot.val().name;
+    wins = checkifnull(wins,childSnapshot,database.ref("players/p2/" + key2));
+    loss = checkifnull(loss,childSnapshot,database.ref("players/p2/" + key2));
+    console.log(player2);
+    console.log(wins);
+    console.log(loss);
+  },function(errorObject){
+    console.log("errors: " + errorObject.code);
+  })
 
 })
